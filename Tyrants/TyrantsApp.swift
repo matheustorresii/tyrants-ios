21841a8b4eeb78a0996409d930b32a5209c3b0fd
matheustorresii <matheustf2003@gmail.com>
2025-10-01 14:44:31 -0300

    Initial Commit

---
+++ Tyrants/TyrantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyrantsApp.swift
+//  Tyrants
+//
+//  Created by Torres on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TyrantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TyrantsTests/TyrantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TyrantsTests.swift
+//  TyrantsTests
+//
+//  Created by Torres on 01/10/25.
+//
+
+import Testing
+@testable import Tyrants
+
+struct TyrantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
